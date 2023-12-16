37a5b103a3c63f24e929ade41bfc30822bdb89e1
Kostyantyn Onischenko <Onischenko411@gmail.com>
2023-12-16 04:27:33 +0200

    change file

---
+++ js.js
@@ -0,0 +1,12 @@
+
+function removeElement ( array, item){
+    for( let i = array.length -1; i >= 0; i--){
+      if(array[i] === item){
+        array.splice(i,1)
+      }
+    }
+};
+const array = [1, 2, 5, 3, '5', 4, 5, 6, '7'];
+removeElement(array, 5);
+debugger
+console.log(array);
